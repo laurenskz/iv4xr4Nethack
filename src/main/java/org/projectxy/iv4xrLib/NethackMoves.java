ffe708e11b05a8e8ac1b189213a9781a666a1ddd
Wish <>
2021-03-19 18:16:10 +0100

    initial setup to construct WorldModel

--- src/main/java/org/projectxy/iv4xrLib/NethackMoves.java
+++ src/main/java/org/projectxy/iv4xrLib/NethackMoves.java
@@ -1,16 +1,20 @@
 package org.projectxy.iv4xrLib;
 
+import A.B.Item;
+import A.B.ItemTile;
+import A.B.Mob;
+import A.B.Monster;
+import A.B.Screen;
+import A.B.Tile;
+import A.B.Weapon;
+import eu.iv4xr.framework.mainConcepts.WorldEntity;
+import eu.iv4xr.framework.mainConcepts.WorldModel;
+import eu.iv4xr.framework.spatial.Vec3;
+
 public class NethackMoves {
 
-
-    private Tile[][] tiles;
-    private Player p1;
-    private PlayerStatus ps;
-    private boolean inventoryScreen, mainMenu, revealControls;
-    private boolean playerTurn,aimingBow;
-
-    int x= p1.getX();   // X position of the avatar
-    int y = p1.getY();  // Y position of the avatar
+    Screen nethack ;
+    
 
     /**
      * Environment needs to set up first, via mapSetup() method.
@@ -44,9 +48,48 @@ public class NethackMoves {
      *
      */
 
+    
+    WorldModel getNetHackState() {
+        
+        WorldModel wom = new WorldModel() ;
+        wom.agentId = "player" ;
+        wom.position = new Vec3(nethack.p1.getX(), nethack.p1.getY(), 0) ;
+        wom.timestamp = nethack.moves ;
+        for(Monster monster : nethack.mobs) {
+            WorldEntity e = new WorldEntity(monster.ID, monster.image, true ) ;
+            e.position = new Vec3(monster.getX(), monster.getY(), 0) ;
+            e.properties.put("health",monster.health) ;
+            e.properties.put("attackDmg",monster.attackDmg) ;
+            wom.elements.put(e.id, e) ; 
+        }
+        for(ItemTile item : nethack.items) {
+          // do something similar
+        }        
+        WorldEntity inv = new WorldEntity("Inventory", "Inventory", true) ;
+        for(Item item : nethack.ps.inventory) {
+            WorldEntity item_ = convertItem(item) ;
+            // add properties ...
+            inv.elements.put(item_.id,item_) ;
+        }
+        wom.elements.put(inv.id,inv) ;
+        return wom ;
+    }
+    
+    
+    WorldEntity convertItem(Item item) {
+        WorldEntity item_ = new WorldEntity(item.ID, item.getClass().getSimpleName(),true) ;
+        if (item instanceof Weapon) {
+            Weapon w = (Weapon) item ;
+            // add weapon properties...
+        }
+        
+        return item_ ;
+    }
+    
+    
 
     // Up
-    public void up() {
+    public WorldModel up() {
         if(ps.getAlive() && !inventoryScreen && !aimingBow && playerTurn)
         {
             if(tiles[x][y-1] instanceof FloorTile || tiles[x][y-1] instanceof ItemTile){
@@ -64,6 +107,7 @@ public class NethackMoves {
 
            // break;
         }
+        return getNetHackState() ;
     }
 
 
